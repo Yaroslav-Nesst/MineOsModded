859d4b82a0242d4a5415ba5ab65b0d7213db5b78
Yaroslav-Nesst <59181976+Yaroslav-Nesst@users.noreply.github.com>
2021-09-26 00:11:01 +0600

    Update Full.lua

--- EFI/Full.lua
+++ EFI/Full.lua
@@ -269,7 +269,7 @@ while uptime() < deadline do
 
 		if internetAddress then	
 			tableInsert(utilities, 2, menuElement("Восстановление через интернет", function()
-				local handle, data, result, reason = componentProxy(internetAddress).request("https://raw.githubusercontent.com/IgorTimofeev/MineOS/master/Installer/Main.lua"), ""
+				local handle, data, result, reason = componentProxy(internetAddress).request("https://raw.githubusercontent.com/Yaroslav-Nesst/MineOsModded/master/Installer/Main.lua"), ""
 
 				if handle then
 					status(stringsMain, "Скачивание скрипта...")
